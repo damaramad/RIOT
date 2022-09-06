2a34e4af529ca6a70b41df92daa730ef095d1d65
Damien Amara <damien.amara@univ-lille.fr>
2022-09-06 18:01:36 +0200

    cpu/nrf5x_common_pip: replace memory-mapped register accesses
    Replace memory-mapped register accesses of the nRF5x real-time counter
    driver by Pip register accesses.

--- cpu/nrf5x_common_pip/periph/rtt.c
+++ cpu/nrf5x_common_pip/periph/rtt.c
@@ -24,14 +24,15 @@
 #include "cpu.h"
 #include "nrf_clock.h"
 #include "periph/rtt.h"
+#include "svc.h"
 
 /* get the IRQ configuration */
 #if (RTT_DEV == 1)
-#define DEV             NRF_RTC1
+#define DEV             PIP_NRF_RTC_RTC1_BASE
 #define ISR             isr_rtc1
 #define IRQn            RTC1_IRQn
 #elif (RTT_DEV == 2)
-#define DEV             NRF_RTC2
+#define DEV             PIP_NRF_RTC_RTC2_BASE
 #define ISR             isr_rtc2
 #define IRQn            RTC2_IRQn
 #else
@@ -50,79 +51,89 @@ void rtt_init(void)
     clock_start_lf();
     /* make sure device is powered */
 #ifdef CPU_FAM_NRF51
-    DEV->POWER = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_POWER_INDEX, 1);
 #endif
     /* stop the RTT during configuration */
-    DEV->TASKS_STOP = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_TASKS_STOP_INDEX, 1);
     /* configure interrupt */
     NVIC_EnableIRQ(IRQn);
     /* set prescaler */
-    DEV->PRESCALER = (RTT_CLOCK_FREQUENCY / RTT_FREQUENCY) - 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_PRESCALER_INDEX, (RTT_CLOCK_FREQUENCY / RTT_FREQUENCY) - 1);
     /* start the actual RTT thing */
-    DEV->TASKS_START = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_TASKS_START_INDEX, 1);
 }
 
 void rtt_set_overflow_cb(rtt_cb_t cb, void *arg)
 {
     overflow_cb = cb;
     overflow_arg = arg;
-    DEV->INTENSET = RTC_INTENSET_OVRFLW_Msk;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_INTENSET_INDEX, RTC_INTENSET_OVRFLW_Msk);
 }
 
 void rtt_clear_overflow_cb(void)
 {
-    DEV->INTENCLR = RTC_INTENCLR_OVRFLW_Msk;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_INTENCLR_INDEX, RTC_INTENCLR_OVRFLW_Msk);
 }
 
 uint32_t rtt_get_counter(void)
 {
-    return DEV->COUNTER;
+    uint32_t reg;
+
+    Pip_in(DEV + PIP_NRF_RTC_RTC1_COUNTER_INDEX, &reg);
+    return reg;
 }
 
 void rtt_set_alarm(uint32_t alarm, rtt_cb_t cb, void *arg)
 {
     alarm_cb = cb;
     alarm_arg = arg;
-    DEV->CC[0] = (alarm & RTT_MAX_VALUE);
-    DEV->INTENSET = RTC_INTENSET_COMPARE0_Msk;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_CC_0_INDEX, (alarm & RTT_MAX_VALUE));
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_INTENSET_INDEX, RTC_INTENSET_COMPARE0_Msk);
 }
 
 uint32_t rtt_get_alarm(void)
 {
-    return DEV->CC[0];
+    uint32_t reg;
+
+    Pip_in(DEV + PIP_NRF_RTC_RTC1_CC_0_INDEX, &reg);
+    return reg;
 }
 
 void rtt_clear_alarm(void)
 {
-    DEV->INTENCLR = RTC_INTENSET_COMPARE0_Msk;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_INTENCLR_INDEX, RTC_INTENSET_COMPARE0_Msk);
 }
 
 void rtt_poweron(void)
 {
 #ifdef CPU_FAM_NRF51
-    DEV->POWER = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_POWER_INDEX, 1);
 #endif
-    DEV->TASKS_START = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_TASKS_START_INDEX, 1);
 }
 
 void rtt_poweroff(void)
 {
-    DEV->TASKS_STOP = 1;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_TASKS_STOP_INDEX, 1);
 #ifdef CPU_FAM_NRF51
-    DEV->POWER = 0;
+    Pip_out(DEV + PIP_NRF_RTC_RTC1_POWER_INDEX, 0);
 #endif
 }
 
 void ISR(void)
 {
-    if (DEV->EVENTS_COMPARE[0] == 1) {
-        DEV->EVENTS_COMPARE[0] = 0;
-        DEV->INTENCLR = RTC_INTENSET_COMPARE0_Msk;
+    uint32_t reg;
+
+    Pip_in(DEV + PIP_NRF_RTC_RTC1_EVENTS_COMPARE_0_INDEX, &reg);
+    if (reg == 1) {
+        Pip_out(DEV + PIP_NRF_RTC_RTC1_EVENTS_COMPARE_0_INDEX, 0);
+        Pip_out(DEV + PIP_NRF_RTC_RTC1_INTENCLR_INDEX, RTC_INTENSET_COMPARE0_Msk);
         alarm_cb(alarm_arg);
     }
 
-    if (DEV->EVENTS_OVRFLW == 1) {
-        DEV->EVENTS_OVRFLW = 0;
+    Pip_in(DEV + PIP_NRF_RTC_RTC1_EVENTS_OVRFLW_INDEX, &reg);
+    if (reg == 1) {
+        Pip_out(DEV + PIP_NRF_RTC_RTC1_EVENTS_OVRFLW_INDEX, 0);
         overflow_cb(overflow_arg);
     }
 
