9fdf56dedc69e1ecf0af90877375042679e22cc5
Damien Amara <damien.amara@univ-lille.fr>
2022-09-07 10:09:04 +0200

    cpu/nrf52_pip: replace memory-mapped register accesses
    Replace memory-mapped register accesses of the cpu.c file by Pip
    register accesses.

--- cpu/nrf52_pip/cpu.c
+++ cpu/nrf52_pip/cpu.c
@@ -29,6 +29,8 @@
 #include "periph/init.h"
 #include "stdio_base.h"
 
+#include "svc.h"
+
 /* FTPAN helper functions */
 static bool ftpan_32(void);
 static bool ftpan_37(void);
@@ -39,23 +41,27 @@ static bool ftpan_36(void);
  */
 void cpu_init(void)
 {
+    uint32_t reg;
+
     /* Workaround for FTPAN-32
      * "DIF: Debug session automatically enables TracePort pins." */
     if (ftpan_32()) {
-        CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk;
+        Pip_in(PIP_ARMV7M_SCS_SCID_DEMCR, &reg);
+        reg &= ~CoreDebug_DEMCR_TRCENA_Msk;
+        Pip_out(PIP_ARMV7M_SCS_SCID_DEMCR, reg);
     }
 
     /* Workaround for FTPAN-37
      * "AMLI: EasyDMA is slow with Radio, ECB, AAR and CCM." */
     if (ftpan_37()) {
-        *(volatile uint32_t *)0x400005A0 = 0x3;
+        Pip_out(PIP_NRF_RADIO_RADIO_ERRATA_32, 0x3);
     }
 
     /* Workaround for FTPAN-36
      * "CLOCK: Some registers are not reset when expected." */
     if (ftpan_36()) {
-        NRF_CLOCK->EVENTS_DONE = 0;
-        NRF_CLOCK->EVENTS_CTTO = 0;
+        Pip_in(PIP_NRF_CLOCK_CLOCK_EVENTS_DONE, 0);
+        Pip_in(PIP_NRF_CLOCK_CLOCK_EVENTS_CTTO, 0);
     }
     /* Enable the DC/DC power converter */
     nrfx_dcdc_init();
@@ -65,14 +71,16 @@ void cpu_init(void)
 
 #ifdef NVMC_ICACHECNF_CACHEEN_Msk
     /* enable instruction cache */
-    NRF_NVMC->ICACHECNF = (NVMC_ICACHECNF_CACHEEN_Msk);
+    Pip_out(PIP_NRF_NVMC_NVMC_ICACHECNF, NVMC_ICACHECNF_CACHEEN_Msk);
 #endif
 
     /* call cortexm default initialization */
     cortexm_init();
 
     /* enable wake up on events for __WFE CPU sleep */
-    SCB->SCR |= SCB_SCR_SEVONPEND_Msk;
+    Pip_in(PIP_ARMV7M_SCS_SCID_SCR, &reg);
+    reg |= SCB_SCR_SEVONPEND_Msk;
+    Pip_out(PIP_ARMV7M_SCS_SCID_SCR, reg);
 
     /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
     stdio_init();
@@ -86,12 +94,19 @@ void cpu_init(void)
  */
 static bool ftpan_32(void)
 {
-    if ((((*(uint32_t *)0xF0000FE0) & 0x000000FF) == 0x6)
-        && (((*(uint32_t *)0xF0000FE4) & 0x0000000F) == 0x0)) {
-        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x30)
-            && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
+    uint32_t reg;
+    Pip_in(PIP_NRF_ERRATA_REG0, &reg);
+    if ((reg & 0x000000FF) == 0x6) {
+      Pip_in(PIP_NRF_ERRATA_REG1, &reg);
+      if ((reg & 0x0000000F) == 0x0) {
+        Pip_in(PIP_NRF_ERRATA_REG2, &reg);
+        if ((reg & 0x000000F0) == 0x30) {
+          Pip_in(PIP_NRF_ERRATA_REG3, &reg);
+          if ((reg & 0x000000F0) == 0x0) {
             return true;
+	  }
         }
+      }
     }
     return false;
 }
@@ -101,12 +116,19 @@ static bool ftpan_32(void)
  */
 static bool ftpan_36(void)
 {
-    if ((((*(uint32_t *)0xF0000FE0) & 0x000000FF) == 0x6)
-        && (((*(uint32_t *)0xF0000FE4) & 0x0000000F) == 0x0)) {
-        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x30)
-            && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
+    uint32_t reg;
+    Pip_in(PIP_NRF_ERRATA_REG0, &reg);
+    if ((reg & 0x000000FF) == 0x6) {
+      Pip_in(PIP_NRF_ERRATA_REG1, &reg);
+      if ((reg & 0x0000000F) == 0x0) {
+        Pip_in(PIP_NRF_ERRATA_REG2, &reg);
+        if ((reg & 0x000000F0) == 0x30) {
+          Pip_in(PIP_NRF_ERRATA_REG3, &reg);
+          if ((reg & 0x000000F0) == 0x0) {
             return true;
+	  }
         }
+      }
     }
     return false;
 }
@@ -116,12 +138,19 @@ static bool ftpan_36(void)
  */
 static bool ftpan_37(void)
 {
-    if ((((*(uint32_t *)0xF0000FE0) & 0x000000FF) == 0x6)
-        && (((*(uint32_t *)0xF0000FE4) & 0x0000000F) == 0x0)) {
-        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x30)
-            && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
+    uint32_t reg;
+    Pip_in(PIP_NRF_ERRATA_REG0, &reg);
+    if ((reg & 0x000000FF) == 0x6) {
+      Pip_in(PIP_NRF_ERRATA_REG1, &reg);
+      if ((reg & 0x0000000F) == 0x0) {
+        Pip_in(PIP_NRF_ERRATA_REG2, &reg);
+        if ((reg & 0x000000F0) == 0x30) {
+          Pip_in(PIP_NRF_ERRATA_REG3, &reg);
+          if ((reg & 0x000000F0) == 0x0) {
             return true;
+	  }
         }
+      }
     }
     return false;
 }
