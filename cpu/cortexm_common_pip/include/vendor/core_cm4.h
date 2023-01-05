f189daa3ec22fa95a10aed87b77ae97e90824f86
Damien Amara <damien.amara@univ-lille.fr>
2023-01-05 18:14:21 +0100

    cpu/cortexm_common_pip: replace memory-mapped register accesses
    Replace memory-mapped register accesses of the __NVIC_SystemReset
    function by Pip register accesses.

--- cpu/cortexm_common_pip/include/vendor/core_cm4.h
+++ cpu/cortexm_common_pip/include/vendor/core_cm4.h
@@ -1951,9 +1951,9 @@ __NO_RETURN __STATIC_INLINE void __NVIC_SystemReset(void)
 {
   __DSB();                                                          /* Ensure all outstanding memory accesses included
                                                                        buffered write are completed before reset */
-  SCB->AIRCR  = (uint32_t)((0x5FAUL << SCB_AIRCR_VECTKEY_Pos)    |
-                           (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) |
-                            SCB_AIRCR_SYSRESETREQ_Msk    );         /* Keep priority group unchanged */
+  Pip_out(PIP_ARMV7M_SCS_SCID_AIRCR, (uint32_t)((0x5FAUL << SCB_AIRCR_VECTKEY_Pos)      |
+                           (Pip_in(PIP_ARMV7M_SCS_SCID_AIRCR) & SCB_AIRCR_PRIGROUP_Msk) |
+                            SCB_AIRCR_SYSRESETREQ_Msk    ));         /* Keep priority group unchanged */
   __DSB();                                                          /* Ensure completion of memory access */
 
   for(;;)                                                           /* wait until reset */
