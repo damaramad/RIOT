e4f5bbbc881574aeec7a07654c85e6ec7c3b1e44
Damien Amara <damien.amara@univ-lille.fr>
2023-04-27 13:51:21 +0200

    cpu/cortexm_common_pip/include: increase the default stack size
    Increase the stack size of threads by sizeof(extendedContext_t) since a
    space of this size is reserved for saving FPU context. This allows to
    have a similar stack size for the threads to the default RIOT version.

--- cpu/cortexm_common_pip/include/cpu_conf_common.h
+++ cpu/cortexm_common_pip/include/cpu_conf_common.h
@@ -19,6 +19,8 @@
 #ifndef CPU_CONF_COMMON_H
 #define CPU_CONF_COMMON_H
 
+#include "context.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -41,7 +43,7 @@ extern "C" {
 #define THREAD_EXTRA_STACKSIZE_PRINTF   (512)
 #endif
 #ifndef THREAD_STACKSIZE_DEFAULT
-#define THREAD_STACKSIZE_DEFAULT        (1024)
+#define THREAD_STACKSIZE_DEFAULT        (1024 + sizeof(extendedContext_t))
 #endif
 #ifndef THREAD_STACKSIZE_IDLE
 #define THREAD_STACKSIZE_IDLE           (256)
